9cbc5e29d36f345381abe5efb2f8717c8c7d5f85
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2025-10-23 18:50:16 +0000

    aulas iniciadas

---
+++ modulo1-typescript-basico/01_types_primitivos.ts
@@ -0,0 +1,16 @@
+// =========================================================
+/*
+    Tipagens primitivas do Typescript: string, number e
+    boolean. Erros e boas práticas.
+*/
+// =========================================================
+
+// =========================================================
+// = = = = = = = = = = // 1-As três principais tipagens
+// =========================================================
+let nome: string = "John"
+let idade: number = 34
+let programador: boolean = true
+// os tipos das variáveis sempre devem ser informadas
+
+console.log({ nome, idade, programador })
